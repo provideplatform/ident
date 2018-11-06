65bb19ee2e87fa22aceedd8405205a857eaa661a
Kevin Munc <muncman@gmail.com>
2018-11-05 23:33:32 -0500

    Added Docker and AWS commands for CI.
    - this includes copying much from the Dawn CI script template.

--- scripts/ci-process.sh
+++ scripts/ci-process.sh
@@ -41,6 +41,42 @@ bootstrap_environment()
     echo '....Environment setup complete....'
 }
 
+get_build_info()
+{
+    echo '....Getting build values....'
+    revNumber=$(echo `git rev-list HEAD | wc -l`) # the echo trims leading whitespace
+    gitHash=`git rev-parse --short HEAD`
+    gitBranch=`git rev-parse --abbrev-ref HEAD`
+    buildDate=$(date '+%m.%d.%y')
+    buildTime=$(date '+%H.%M.%S')
+    echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; if [ "$?" -ne "0" ]; then echo 'Local git status is dirty'; fi )";
+    buildRef=${gitBranch}-${gitHash}-${buildDate}-${buildTime}
+    echo 'Build Ref =' $buildRef
+}
+
+perform_deployment()
+{
+    if [[ -z "${ECR_REPOSITORY_NAME}" || -z "${ECS_CLUSTER}" || -z "${ECS_TASK_DEFINITION_FAMILY}" || -z "${ECS_SERVICE_NAME}" ]]
+    then
+        echo '....[PRVD] Skipping container deployment....'
+    else
+        DEFINITION_FILE=ecs-task-definition.json
+        MUNGED_FILE=ecs-task-definition-UPDATED.json
+        echo '....list-images....'
+        ECR_IMAGE_DIGEST=$(aws ecr list-images --repository-name provide/ident | jq '.imageIds[0].imageDigest')
+        echo '....describe-images....'
+        ECR_IMAGE=$(aws ecr describe-images --repository-name "${ECR_REPOSITORY_NAME}" --image-ids imageDigest="${ECR_IMAGE_DIGEST}" | jq '.')
+        echo '....describe-task-definition....'
+        ECS_TASK_DEFINITION=$(aws ecs describe-task-definition --task-definition "${ECS_TASK_DEFINITION_FAMILY}" | jq '.taskDefinition | del(.taskDefinitionArn) | del(.revision) | del(.status) | del(.compatibilities) | del(.requiresAttributes)')
+        echo '....file manipulation....'
+        echo $ECS_TASK_DEFINITION > $DEFINITION_FILE
+        sed -E "s/ident:[a-zA-Z0-9\.-]+/ident:${buildRef}/" "./${DEFINITION_FILE}" > "./${MUNGED_FILE}"
+        echo '....register-task-definition....'
+        ECS_TASK_DEFINITION_ID=$(aws ecs register-task-definition --family "${ECS_TASK_DEFINITION_FAMILY}" --cli-input-json "file://${MUNGED_FILE}" | jq '.taskDefinition.taskDefinitionArn' | sed -E 's/.*\/(.*)"$/\1/')
+        aws ecs update-service --cluster "${ECS_CLUSTER}" --service "${ECS_SERVICE_NAME}" --task-definition "${ECS_TASK_DEFINITION_ID}"
+    fi
+}
+
 # Preparation
 echo '....Running the full continuous integration process....'
 scriptDir=`dirname $0`
@@ -50,6 +86,7 @@ echo 'Working Directory =' `pwd`
 # The Process
 echo '....[PRVD] Setting Up....'
 bootstrap_environment
+get_build_info
 rm ./ident 2>/dev/null || true # silence error if not present
 go fix .
 go fmt
@@ -61,10 +98,15 @@ echo '....[PRVD] Building....'
 go build -v
 echo '....[PRVD] Testing....'
 go test -v -race -cover -html=cover/coverage.cov -o coverage.html ./... # TODO: -msan (for Clang's MemorySanitizer)
-# TODO: build for deployment... go build -o $GOPATH/../build/ident
-# echo '....[PRVD] Docker Build....'
-# echo '....[PRVD] Docker Tag....'
-# echo '....[PRVD] Docker Push....'
+echo '....[PRVD] Docker Build....'
+sudo docker build -t provide/ident .
+echo '....[PRVD] Docker Tag....'
+sudo docker tag provide/ident:latest "085843810865.dkr.ecr.us-east-1.amazonaws.com/provide/ident:${buildRef}"
+echo '....[PRVD] Docker Push....'
+$(aws ecr get-login --no-include-email --region us-east-1)
+sudo docker push "085843810865.dkr.ecr.us-east-1.amazonaws.com/provide/ident:${buildRef}"
+echo '....[PRVD] AWS Deployment....'
+perform_deployment
 
 # Finalization
 popd &>/dev/null
