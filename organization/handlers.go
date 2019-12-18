a4489ec87cd54768dbe7ac559f568db991aba9a0
Kyle Thomas <kyle@provide.services>
2019-12-18 18:21:34 -0500

    Add organizations and organization users list apis

--- organization/handlers.go
+++ organization/handlers.go
@@ -1,7 +1,14 @@
 package organization
 
 import (
+	"encoding/json"
+
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	dbconf "github.com/kthomas/go-db-config"
+	uuid "github.com/kthomas/go.uuid"
+	"github.com/provideapp/ident/token"
+	"github.com/provideapp/ident/user"
 	provide "github.com/provideservices/provide-go"
 )
 
@@ -22,8 +29,29 @@ func InstallOrganizationUsersAPI(r *gin.Engine) {
 	r.DELETE("/api/v1/organizations/:id/users/:userId", deleteOrganizationUserHandler)
 }
 
+func resolveOrganization(db *gorm.DB, appID *uuid.UUID) *gorm.DB {
+	query := db.Joins("applications_organizations as ao ON ao.organization_id = organizations.id")
+	if appID != nil {
+		query = query.Where("ao.application_id = ?", appID)
+	}
+	return query
+}
+
 func organizationsListHandler(c *gin.Context) {
-	provide.RenderError("not implemented", 501, c)
+	bearer := token.InContext(c)
+	applicationID := bearer.ApplicationID
+
+	if applicationID == nil || *applicationID == uuid.Nil {
+		provide.RenderError("unauthorized", 401, c)
+		return
+	}
+
+	var orgs []*Organization
+
+	query := dbconf.DatabaseConnection()
+	query = resolveOrganization(query, applicationID)
+	provide.Paginate(c, query, &Organization{}).Find(&orgs)
+	provide.Render(orgs, 200, c)
 }
 
 func organizationDetailsHandler(c *gin.Context) {
@@ -31,7 +59,47 @@ func organizationDetailsHandler(c *gin.Context) {
 }
 
 func createOrganizationHandler(c *gin.Context) {
-	provide.RenderError("not implemented", 501, c)
+	bearer := token.InContext(c)
+	userID := bearer.UserID
+
+	if userID == nil || *userID == uuid.Nil {
+		provide.RenderError("unauthorized", 401, c)
+		return
+	}
+
+	buf, err := c.GetRawData()
+	if err != nil {
+		provide.RenderError(err.Error(), 400, c)
+		return
+	}
+
+	params := map[string]interface{}{}
+	err = json.Unmarshal(buf, &params)
+	if err != nil {
+		provide.RenderError(err.Error(), 400, c)
+		return
+	}
+
+	org := &Organization{}
+	err = json.Unmarshal(buf, &org)
+	if err != nil {
+		provide.RenderError(err.Error(), 422, c)
+		return
+	}
+	org.UserID = userID
+
+	if _, permissionsOk := params["permissions"]; permissionsOk {
+		provide.RenderError("unable to assert arbitrary organization permissions", 403, c)
+		return
+	}
+
+	if org.Create() {
+		provide.Render(org, 201, c)
+	} else {
+		obj := map[string]interface{}{}
+		obj["errors"] = org.Errors
+		provide.Render(obj, 422, c)
+	}
 }
 
 func updateOrganizationHandler(c *gin.Context) {
@@ -43,7 +111,26 @@ func deleteOrganizationHandler(c *gin.Context) {
 }
 
 func organizationUsersListHandler(c *gin.Context) {
-	provide.RenderError("not implemented", 501, c)
+	bearer := token.InContext(c)
+	applicationID := bearer.ApplicationID
+
+	if applicationID == nil || *applicationID == uuid.Nil {
+		provide.RenderError("unauthorized", 401, c)
+		return
+	}
+
+	org := &Organization{}
+	query := dbconf.DatabaseConnection()
+	query = resolveOrganization(query, applicationID).Find(&org)
+
+	if org == nil || org.ID == uuid.Nil {
+		provide.RenderError("unauthorized", 401, c)
+		return
+	}
+
+	var users []*user.User
+	provide.Paginate(c, query.Model(&org).Related(&users, "Users"), &user.User{}).Find(&users)
+	provide.Render(users, 200, c)
 }
 
 func createOrganizationUserHandler(c *gin.Context) {
