6eb12441e088d314cca7f7e3dcefd7042b5408ef
Kyle Thomas <kyle@provide.services>
2020-01-13 14:35:01 -0500

    Further restrict joined organization user results when listing applications

--- application/handlers.go
+++ application/handlers.go
@@ -51,7 +51,7 @@ func applicationsListHandler(c *gin.Context) {
 	query = query.Where("applications.hidden = ?", hidden)
 
 	query = query.Joins("INNER JOIN applications_organizations as ao ON ao.application_id = applications.id INNER JOIN organizations_users as ou ON ou.organization_id = ao.organization_id")
-	query = query.Where("applications.user_id = ? OR ou.user_id = ?", userID, userID)
+	query = query.Where("ao.organization_id = ou.organization_id AND (applications.user_id = ? OR ou.user_id = ?)", userID, userID)
 
 	if c.Query("network_id") != "" {
 		query = query.Where("applications.network_id = ?", c.Query("network_id"))
