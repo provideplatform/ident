2799407fec35d5efe69fda19ff5fa821c6c45bb8
Kyle Thomas <kyle@provide.services>
2020-02-12 07:50:48 -0500

    Update default authorized NATS subscription subjects

--- token/token.go
+++ token/token.go
@@ -871,7 +871,7 @@ func (t *Token) encodeJWTNatsClaims() (map[string]interface{}, error) {
 	} else {
 		// FIXME-- put these defaults in configuration and read them from there...
 		subscribeAllow = append(subscribeAllow, "network.*.status")
-		subscribeAllow = append(subscribeAllow, "platform.*")
+		subscribeAllow = append(subscribeAllow, "platform.>")
 	}
 
 	var publishPermissions map[string]interface{}
