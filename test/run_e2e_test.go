74f49e398d6b448c58553f2261ee88169266da53
eoinco <eoin.connolly@gmail.com>
2020-10-21 17:51:50 +0100

    added a bunch of user integration tests

--- test/run_e2e_test.go
+++ test/run_e2e_test.go
@@ -22,7 +22,15 @@ func userFactory(firstName, lastName, email, password string) (*provide.User, er
 	return user, err
 }
 
-func tokenFactory(auth string, applicationID uuid.UUID) (*provide.Token, error) {
+func orgTokenFactory(auth string, organizationID uuid.UUID) (*provide.Token, error) {
+
+	token, err := provide.CreateToken(auth, map[string]interface{}{
+		"organization_id": organizationID,
+	})
+	return token, err
+}
+
+func appTokenFactory(auth string, applicationID uuid.UUID) (*provide.Token, error) {
 
 	token, err := provide.CreateToken(auth, map[string]interface{}{
 		"application_id": applicationID,
@@ -47,7 +55,11 @@ func TestCreateUser(t *testing.T) {
 			t.Errorf("user creation failed. Error: %s", err.Error())
 			return
 		}
-		t.Logf("user returned: %+v", user)
+
+		if user == nil {
+			t.Errorf("no user returned")
+			return
+		}
 	}
 }
 
@@ -73,9 +85,10 @@ func TestAuthenticateUser(t *testing.T) {
 		if err != nil {
 			t.Errorf("user authentication failed. Error: %s", err.Error())
 		}
-		t.Logf("user authenticated: %+v", auth)
-		t.Logf("user returned from auth: %+v", *auth.User)
-		t.Logf("token returned from auth: %s", *auth.Token.Token)
+
+		if auth == nil {
+			t.Errorf("user not authenticated")
+		}
 	}
 }
 func TestCreateOrganization(t *testing.T) {
@@ -118,8 +131,6 @@ func TestCreateOrganization(t *testing.T) {
 			t.Errorf("user authentication failed for user %s. error: %s", tc.email, err.Error())
 		}
 
-		//orgSample := provide.Organization{}
-
 		// create the org with that user (for the moment...)
 		org, err := provide.CreateOrganization(string(*auth.Token.Token), map[string]interface{}{
 			"name":        userOrg.name,
@@ -130,7 +141,10 @@ func TestCreateOrganization(t *testing.T) {
 			t.Errorf("error creating organisation for user id %s", user.ID)
 		}
 
-		t.Logf("org created %+v", org)
+		if org == nil {
+			t.Errorf("no org created")
+			return
+		}
 	}
 }
 
@@ -141,15 +155,6 @@ func TestCreateApplication(t *testing.T) {
 		t.Logf("error creating new UUID")
 	}
 
-	type organization struct {
-		name        string
-		description string
-	}
-	userOrg := organization{
-		"Org " + testId.String(),
-		"Org " + testId.String() + " Decription",
-	}
-
 	type application struct {
 		name        string
 		description string
@@ -169,7 +174,6 @@ func TestCreateApplication(t *testing.T) {
 		{"joey", "joe joe", "j.j" + testId.String() + "@email.com", "joeyjoejoe"},
 	}
 
-	// create the users and add them to the organization
 	for _, tc := range tt {
 		user, err := userFactory(tc.firstName, tc.lastName, tc.email, tc.password)
 		if err != nil {
@@ -182,18 +186,6 @@ func TestCreateApplication(t *testing.T) {
 		if err != nil {
 			t.Errorf("user authentication failed for user %s. error: %s", tc.email, err.Error())
 		}
-
-		// create the org with that user
-		org, err := provide.CreateOrganization(string(*auth.Token.Token), map[string]interface{}{
-			"name":        userOrg.name,
-			"description": userOrg.description,
-			"user_id":     user.ID,
-		})
-		if err != nil {
-			t.Errorf("error creating organisation for user id %s", user.ID)
-		}
-
-		t.Logf("TestCreateApplication: org created %+v", org)
 
 		// Create an Application for that org
 		app, err := provide.CreateApplication(string(*auth.Token.Token), map[string]interface{}{
@@ -204,25 +196,22 @@ func TestCreateApplication(t *testing.T) {
 		if err != nil {
 			t.Errorf("error creation application for user id %s", user.ID)
 		}
-		t.Logf("TestCreateApplication: app created %+v", *app)
+
+		if app == nil {
+			t.Errorf("no application created")
+			return
+		}
 	}
 }
 
-func TestListUsers(t *testing.T) {
+func TestListApplicationUsers(t *testing.T) {
+
 	testId, err := uuid.NewV4()
 	if err != nil {
 		t.Logf("error creating new UUID")
 	}
 
-	t.Logf("testId: %s", testId.String())
-	type organization struct {
-		name        string
-		description string
-	}
-	userOrg := organization{
-		"Org " + testId.String(),
-		"Org " + testId.String() + " Decription",
-	}
+	//t.Logf("*** test list users *** using testid %s", testId)
 
 	type application struct {
 		name        string
@@ -241,12 +230,15 @@ func TestListUsers(t *testing.T) {
 	}{
 		{"first", "last", "first.last." + testId.String() + "@email.com", "secrit_password"},
 		{"joey", "joe joe", "j.j" + testId.String() + "@email.com", "joeyjoejoe"},
+		{"joey2", "joe joe2", "j.j2" + testId.String() + "@email.com", "joeyjoejoe2"},
 	}
 
 	appToken := &provide.Token{}
 	userApplication := &provide.Application{}
 
 	for _, tc := range tt {
+
+		// create the user
 		user, err := userFactory(tc.firstName, tc.lastName, tc.email, tc.password)
 		if err != nil {
 			t.Errorf("user creation failed. Error: %s", err.Error())
@@ -257,31 +249,11 @@ func TestListUsers(t *testing.T) {
 		auth, err := provide.Authenticate(tc.email, tc.password)
 		if err != nil {
 			t.Errorf("user authentication failed for user %s. error: %s", tc.email, err.Error())
+			return
 		}
 
-		// create the org with that user
-		org, err := provide.CreateOrganization(string(*auth.Token.Token), map[string]interface{}{
-			"name":        userOrg.name,
-			"description": userOrg.description,
-			"user_id":     user.ID,
-		})
-		if err != nil {
-			t.Errorf("error creating organisation for user id %s", user.ID)
-		}
-
-		t.Logf("TestCreateApplication: org created %+v", org)
-
-		// refresh the auth token
-		auth, err = provide.Authenticate(tc.email, tc.password)
-		if err != nil {
-			t.Errorf("user authentication failed for user %s. error: %s", tc.email, err.Error())
-		}
-
-		t.Logf("** user application name %+v", userApplication)
-		// Create an Application
+		// Create an Application if it doesn't exist
 		if userApplication.Name == nil {
-			t.Logf("*******no user application created, creating...")
-
 			userApplication, err = provide.CreateApplication(string(*auth.Token.Token), map[string]interface{}{
 				"name":        userApp.name,
 				"description": userApp.description,
@@ -289,46 +261,47 @@ func TestListUsers(t *testing.T) {
 			})
 			if err != nil {
 				t.Errorf("error creation application for user id %s", user.ID)
+				return
 			}
 
-			t.Logf("app returned %+v", *userApplication)
-
-		} else {
-			t.Logf("********updating user app with user details")
-
-			// get an auth token which will include the app
-			appToken, err = tokenFactory(*auth.Token.Token, userApplication.ID)
+			// create a token for that application
+			t.Logf("creating token")
+			appToken, err = appTokenFactory(*auth.Token.Token, userApplication.ID)
 			if err != nil {
 				t.Errorf("token creation failed for application id %s. error: %s", userApplication.ID, err.Error())
+				return
 			}
 
-			err = provide.UpdateApplication(*appToken.Token, userApplication.ID.String(), map[string]interface{}{
-				"name":        userApp.name,
-				"description": userApp.description,
-				"user_id":     user.ID,
+		} else {
+			// let's add this user to the application as the creating user is automatically added...
+			// access the add user path through the hackyhack
+			// FIXME should be available in provide-go
+			path := fmt.Sprintf("applications/%s/users/", userApplication.ID.String())
+
+			status, resp, err := provide.InitIdentService(appToken.Token).Post(path, map[string]interface{}{
+				"user_id": user.ID,
 			})
 			if err != nil {
-				t.Errorf("error creation application for user id %s", user.ID)
+				t.Errorf("failed to add user %s to organization %s; status: %v; %s", userApplication.ID.String(), user.ID, status, err.Error())
+				return
 			}
 
-			t.Logf("app returned %+v", *userApplication)
-		}
-
-		// get an auth token which will include the app
-		appToken, err = tokenFactory(*auth.Token.Token, userApplication.ID)
-		if err != nil {
-			t.Errorf("token creation failed for application id %s. error: %s", userApplication.ID, err.Error())
+			if status != 204 {
+				t.Errorf("failed to add user to application; status: %v; resp: %v", status, resp)
+				return
+			}
 		}
 	}
 
 	if appToken != nil {
-		//t.Logf("token, possibly with application id %+v", *appToken.Token)
 		users, err := provide.ListUsers(string(*appToken.Token), map[string]interface{}{})
 		if err != nil {
 			t.Errorf("error getting users list %s", err.Error())
+			return
 		}
 		if len(users) != len(tt) {
 			t.Errorf("incorrect number of users returned, expected %d, got %d", len(tt), len(users))
+			return
 		}
 	}
 }
@@ -349,12 +322,8 @@ func TestUserAccessRefreshToken(t *testing.T) {
 		return
 	}
 
-	// auth, err := provide.Authenticate(email, passwd)
-	// if err != nil {
-	// 	t.Errorf("error authenticating with ident. error %s", err.Error())
-	// }
-
 	// get the auth token
+	// FIXME should be available in provide-go (offline_access param)
 	status, resp, err := provide.InitIdentService(nil).Post("authenticate", map[string]interface{}{
 		"email":    email,
 		"password": passwd,
@@ -515,5 +484,261 @@ func TestOrgAccessRefreshToken(t *testing.T) {
 	if accessToken.ExpiresIn == nil {
 		t.Error("expires_in not returned for refresh_token token grant")
 		return
+	}
+}
+
+func TestListOrganisationUsers(t *testing.T) {
+
+	testId, err := uuid.NewV4()
+	if err != nil {
+		t.Logf("error creating new UUID")
+	}
+
+	//t.Logf("*** test list users *** using testid %s", testId)
+
+	type organization struct {
+		name        string
+		description string
+	}
+	userOrg := organization{
+		"Org " + testId.String(),
+		"Org " + testId.String() + " Decription",
+	}
+
+	tt := []struct {
+		firstName string
+		lastName  string
+		email     string
+		password  string
+	}{
+		{"first", "last", "first.last." + testId.String() + "@email.com", "secrit_password"},
+		{"joey", "joe joe", "j.j" + testId.String() + "@email.com", "joeyjoejoe"},
+		{"joey2", "joe joe2", "j.j2" + testId.String() + "@email.com", "joeyjoejoe2"},
+	}
+
+	appToken := &provide.Token{}
+	userOrganization := &provide.Organization{}
+
+	for _, tc := range tt {
+
+		// create the user
+		user, err := userFactory(tc.firstName, tc.lastName, tc.email, tc.password)
+		if err != nil {
+			t.Errorf("user creation failed. Error: %s", err.Error())
+			return
+		}
+
+		// get the auth token
+		auth, err := provide.Authenticate(tc.email, tc.password)
+		if err != nil {
+			t.Errorf("user authentication failed for user %s. error: %s", tc.email, err.Error())
+			return
+		}
+
+		// Create an Organization if it doesn't exist
+		if userOrganization.Name == nil {
+
+			userOrganization, err = provide.CreateOrganization(string(*auth.Token.Token), map[string]interface{}{
+				"name":        userOrg.name,
+				"description": userOrg.description,
+				"user_id":     user.ID,
+			})
+			if err != nil {
+				t.Errorf("error creation organization for user id %s", user.ID)
+				return
+			}
+
+			// create a token for that organization
+			appToken, err = orgTokenFactory(*auth.Token.Token, userOrganization.ID)
+			if err != nil {
+				t.Errorf("token creation failed for organization id %s. error: %s", userOrganization.ID, err.Error())
+				return
+			}
+
+		} else {
+			//let's add this user to the organization as the creating user is automatically added...
+
+			// access the add user path through the hackyhack
+			//FIXME should be available in provide-go
+			path := fmt.Sprintf("organizations/%s/users/", userOrganization.ID.String())
+
+			status, resp, err := provide.InitIdentService(appToken.Token).Post(path, map[string]interface{}{
+				"user_id": user.ID,
+			})
+			if err != nil {
+				t.Errorf("failed to add user %s to organization %s; status: %v; %s", userOrganization.ID.String(), user.ID, status, err.Error())
+				return
+			}
+
+			if status != 204 {
+				t.Errorf("failed to add user to organization; status: %v; resp: %v", status, resp)
+				return
+			}
+		}
+	}
+
+	if appToken != nil {
+		users, err := provide.ListUsers(string(*appToken.Token), map[string]interface{}{})
+		if err != nil {
+			t.Errorf("error getting users list %s", err.Error())
+		}
+		if len(users) != len(tt) {
+			t.Errorf("incorrect number of users returned, expected %d, got %d", len(tt), len(users))
+		}
+		t.Logf("got correct number of organization users back %d", len(users))
+	}
+}
+
+func TestUserDetails(t *testing.T) {
+
+	testId, err := uuid.NewV4()
+	if err != nil {
+		t.Logf("error creating new UUID")
+	}
+
+	tt := []struct {
+		firstName string
+		lastName  string
+		email     string
+		password  string
+	}{
+		{"first", "last", "first.last." + testId.String() + "@email.com", "secrit_password"},
+		{"joey", "joe joe", "j.j" + testId.String() + "@email.com", "joeyjoejoe"},
+		{"joey2", "joe joe2", "j.j2" + testId.String() + "@email.com", "joeyjoejoe2"},
+	}
+
+	for _, tc := range tt {
+		// create the user
+		user, err := userFactory(tc.firstName, tc.lastName, tc.email, tc.password)
+		if err != nil {
+			t.Errorf("user creation failed. Error: %s", err.Error())
+			return
+		}
+
+		// get the auth token
+		auth, err := provide.Authenticate(tc.email, tc.password)
+		if err != nil {
+			t.Errorf("user authentication failed for user %s. error: %s", tc.email, err.Error())
+			return
+		}
+
+		// get the user details
+		deets, err := provide.GetUserDetails(*auth.Token.Token, user.ID.String(), map[string]interface{}{})
+		if err != nil {
+			t.Errorf("error getting details for user id %s. Error: %s", user.ID, err.Error())
+			return
+		}
+
+		// fail if they're not the same!
+		if deets.Email != user.Email {
+			t.Errorf("user email not returned correctly. expected %s, got %s", user.Email, deets.Email)
+			return
+		}
+
+		if deets.FirstName != user.FirstName {
+			t.Errorf("user first name not returned correctly. expected %s, got %s", user.FirstName, deets.FirstName)
+			return
+		}
+
+		if deets.LastName != user.LastName {
+			t.Errorf("user last name not returned correctly. expected %s, got %s", user.LastName, deets.LastName)
+			return
+		}
+
+		if deets.Name != user.Name {
+			t.Errorf("user name not returned correctly. expected %s, got %s", user.Name, deets.Name)
+			return
+		}
+	}
+}
+
+func TestUserUpdate(t *testing.T) {
+	testId, err := uuid.NewV4()
+	if err != nil {
+		t.Logf("error creating new UUID")
+	}
+
+	tt := []struct {
+		firstName string
+		lastName  string
+		email     string
+		password  string
+	}{
+		{"first", "last", "first.last." + testId.String() + "@email.com", "secrit_password"},
+		{"joey", "joe joe", "j.j" + testId.String() + "@email.com", "joeyjoejoe"},
+		{"joey2", "joe joe2", "j.j2" + testId.String() + "@email.com", "joeyjoejoe2"},
+	}
+
+	for _, tc := range tt {
+		// create the user
+		user, err := userFactory(tc.firstName, tc.lastName, tc.email, tc.password)
+		if err != nil {
+			t.Errorf("user creation failed. Error: %s", err.Error())
+			return
+		}
+
+		// get the auth token
+		auth, err := provide.Authenticate(tc.email, tc.password)
+		if err != nil {
+			t.Errorf("user authentication failed for user %s. error: %s", tc.email, err.Error())
+			return
+		}
+
+		// update the user's details
+		updatedFirstName := fmt.Sprintf("%s%s", tc.firstName, testId.String())
+		updatedLastName := fmt.Sprintf("%s%s", tc.lastName, testId.String())
+		updatedName := fmt.Sprintf("%s%s %s%s", tc.firstName, testId.String(), tc.lastName, testId.String())
+		updatedEmail := fmt.Sprintf("%s%s", tc.email, testId.String())
+		updatedPassword := fmt.Sprintf("%s%s", tc.password, testId.String())
+
+		err = provide.UpdateUser(*auth.Token.Token, user.ID.String(), map[string]interface{}{
+			"first_name": updatedFirstName,
+			"last_name":  updatedLastName,
+			"email":      updatedEmail,
+			"password":   updatedPassword,
+		})
+		if err != nil {
+			t.Errorf("error updating user details. Error: %s", err.Error())
+		}
+
+		// get the user details
+		deets, err := provide.GetUserDetails(*auth.Token.Token, user.ID.String(), map[string]interface{}{})
+		if err != nil {
+			t.Errorf("error getting details for user id %s. Error: %s", user.ID, err.Error())
+			return
+		}
+
+		if deets.FirstName != updatedFirstName {
+			t.Errorf("user first name not returned correctly. expected %s, got %s", updatedFirstName, deets.FirstName)
+			return
+		}
+
+		if deets.LastName != updatedLastName {
+			t.Errorf("user last name not returned correctly. expected %s, got %s", updatedLastName, deets.LastName)
+			return
+		}
+
+		if deets.Name != updatedName {
+			t.Errorf("user name not returned correctly. expected %s, got %s", updatedName, deets.Name)
+			return
+		}
+
+		if deets.Email != updatedEmail {
+			t.Errorf("user email not returned correctly. expected %s, got %s", updatedEmail, deets.Name)
+			return
+		}
+
+		//check the updated password
+		auth, err = provide.Authenticate(updatedEmail, updatedPassword)
+		if err != nil {
+			t.Errorf("user authentication failed for user %s. error: %s", updatedEmail, err.Error())
+			return
+		}
+
+		if auth.Token == nil {
+			t.Errorf("no token returned for updated user %s", updatedEmail)
+			return
+		}
+
 	}
 }
