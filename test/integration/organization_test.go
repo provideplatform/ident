ad5890ebde4617813d2397f5f7d7b6141148e4a3
Kyle Thomas <kyle@provide.services>
2020-10-22 02:40:43 -0400

    Bump provide-go and refactor failing organization user association test case
    Still failing as of this commit...

--- test/integration/organization_test.go
+++ test/integration/organization_test.go
@@ -3,7 +3,6 @@
 package integration
 
 import (
-	"fmt"
 	"testing"
 
 	uuid "github.com/kthomas/go.uuid"
@@ -67,14 +66,18 @@ func TestCreateOrganization(t *testing.T) {
 	}
 }
 
-func TestListOrganisationUsers(t *testing.T) {
+// users must have sufficient permission on the organization to create and list users...
+// an org token does not current grant access to the list of users-- it is more intended
+// to be a programmatic api token for interacting with organization-owned resources *without*
+// undermining the privacy of users who may be part of the organization...
+func TestListOrganizationUsers(t *testing.T) {
 
 	testId, err := uuid.NewV4()
 	if err != nil {
 		t.Logf("error creating new UUID")
 	}
 
-	//t.Logf("*** test list users *** using testid %s", testId)
+	// t.Logf("*** test list users *** using testid %s", testId)
 
 	type organization struct {
 		name        string
@@ -96,11 +99,10 @@ func TestListOrganisationUsers(t *testing.T) {
 		{"joey2", "joe joe2", "j.j2" + testId.String() + "@email.com", "joeyjoejoe2"},
 	}
 
-	appToken := &provide.Token{}
-	userOrganization := &provide.Organization{}
+	var org *provide.Organization
+	var organizingUserToken *provide.Token
 
 	for _, tc := range tt {
-
 		// create the user
 		user, err := userFactory(tc.firstName, tc.lastName, tc.email, tc.password)
 		if err != nil {
@@ -115,56 +117,38 @@ func TestListOrganisationUsers(t *testing.T) {
 			return
 		}
 
-		// Create an Organization if it doesn't exist
-		if userOrganization.Name == nil {
+		if organizingUserToken == nil {
+			organizingUserToken = auth.Token
+		}
 
-			userOrganization, err = provide.CreateOrganization(string(*auth.Token.Token), map[string]interface{}{
+		// Create an Organization if it doesn't exist
+		if org == nil {
+			org, err = provide.CreateOrganization(string(*auth.Token.Token), map[string]interface{}{
 				"name":        userOrg.name,
 				"description": userOrg.description,
-				"user_id":     user.ID,
 			})
 			if err != nil {
 				t.Errorf("error creation organization for user id %s", user.ID)
 				return
 			}
-
-			// create a token for that organization
-			appToken, err = orgTokenFactory(*auth.Token.Token, userOrganization.ID)
-			if err != nil {
-				t.Errorf("token creation failed for organization id %s. error: %s", userOrganization.ID, err.Error())
-				return
-			}
-
 		} else {
-			//let's add this user to the organization as the creating user is automatically added...
-
-			// access the add user path through the hackyhack
-			//FIXME should be available in provide-go
-			path := fmt.Sprintf("organizations/%s/users/", userOrganization.ID.String())
-
-			status, resp, err := provide.InitIdentService(appToken.Token).Post(path, map[string]interface{}{
-				"user_id": user.ID,
+			// let's add this user to the organization as the creating user is automatically added...
+			err := provide.CreateOrganizationUser(*organizingUserToken.Token, org.ID.String(), map[string]interface{}{
+				"user_id": user.ID.String(),
 			})
 			if err != nil {
-				t.Errorf("failed to add user %s to organization %s; status: %v; %s", userOrganization.ID.String(), user.ID, status, err.Error())
-				return
-			}
-
-			if status != 204 {
-				t.Errorf("failed to add user to organization; status: %v; resp: %v", status, resp)
+				t.Errorf("failed to add user %s to organization %s; %s", user.ID, org.ID.String(), err.Error())
 				return
 			}
 		}
 	}
 
-	if appToken != nil {
-		users, err := provide.ListUsers(string(*appToken.Token), map[string]interface{}{})
-		if err != nil {
-			t.Errorf("error getting users list %s", err.Error())
-		}
-		if len(users) != len(tt) {
-			t.Errorf("incorrect number of users returned, expected %d, got %d", len(tt), len(users))
-		}
-		t.Logf("got correct number of organization users back %d", len(users))
+	users, err := provide.ListOrganizationUsers(string(*organizingUserToken.Token), org.ID.String(), map[string]interface{}{})
+	if err != nil {
+		t.Errorf("error getting organization users list %s", err.Error())
 	}
+	if len(users) != len(tt) {
+		t.Errorf("incorrect number of organization users returned, expected %d, got %d", len(tt), len(users))
+	}
+	t.Logf("got correct number of organization users back %d", len(users))
 }
