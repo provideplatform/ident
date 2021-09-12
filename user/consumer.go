97616b1055f0e8b1d0735c3ce7d138415f33f0b1
Kyle Thomas <kyle@provide.services>
2021-09-12 18:04:39 -0400

    Revert "Stop panic if invitation jetstream subscription fails"
    This reverts commit 5200168c10f89a9cbc4fc1354201b2da7f7b208b.

--- user/consumer.go
+++ user/consumer.go
@@ -37,7 +37,7 @@ func init() {
 
 func createNatsDispatchInvitationSubscriptions(wg *sync.WaitGroup) {
 	for i := uint64(0); i < natsutil.GetNatsConsumerConcurrency(); i++ {
-		natsutil.RequireNatsJetstreamSubscription(wg,
+		_, err := natsutil.RequireNatsJetstreamSubscription(wg,
 			dispatchInvitationAckWait,
 			natsDispatchInvitationSubject,
 			natsDispatchInvitationSubject,
@@ -48,6 +48,10 @@ func createNatsDispatchInvitationSubscriptions(wg *sync.WaitGroup) {
 			dispatchInvitationMaxDeliveries,
 			nil,
 		)
+
+		if err != nil {
+			common.Log.Panicf("failed to subscribe to NATS stream via subject: %s; %s", natsDispatchInvitationSubject, err.Error())
+		}
 	}
 }
 
