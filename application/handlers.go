f4a9893388326eb286ec3d0da765a3d0f3ee7b79
Kyle Thomas <kyle@provide.services>
2021-12-22 02:35:25 -0500

    Fix build

--- application/handlers.go
+++ application/handlers.go
@@ -259,7 +259,7 @@ func updateApplicationHandler(c *gin.Context) {
 		return
 	}
 
-	if (bearer.UserID != nil && *bearer.UserID != app.UserID) && (app.OrganizationID == nil || bearer.OrganizationID == nil && ) {
+	if (bearer.UserID != nil && *bearer.UserID != app.UserID) && (app.OrganizationID == nil || bearer.OrganizationID == nil) {
 		provide.RenderError("forbidden", 403, c)
 		return
 	}
