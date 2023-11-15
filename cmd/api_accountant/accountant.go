dda91c7382a6d0ca4d4f76acbde8ecce43571bc9
kt <kyle@provide.services>
2023-11-15 18:33:11 -0500

    Update api accounting logic

--- cmd/api_accountant/accountant.go
+++ cmd/api_accountant/accountant.go
@@ -47,12 +47,13 @@ type siaAPICall struct {
 	Sha256        *string   `json:"sha256,omitempty"`
 	UserAgent     *string   `json:"user_agent,omitempty"`
 
-	// sia types
-	AccountID     *uint //`json:"account_id"`
-	ApplicationID *uint //`json:"application_id"`
+	Hash *string `gorm:"column:sha256" json:"sha256"`
+	Raw  []byte  `json:"raw"`
 
-	Hash *string         `gorm:"-"`
-	Raw  json.RawMessage `json:"raw"`
+	// sia types
+	AccountID      *uint //`json:"account_id"`
+	ApplicationID  *uint //`json:"application_id"`
+	OrganizationID *uint //`json:"organization_id"`
 }
 
 // CalculateHash calculates the sha256 hash of the APICall instance using
@@ -82,12 +83,13 @@ func (siaAPICall) TableName() string {
 
 type siaAccount struct {
 	SiaModel
-	Name          *string    `json:"name"`
-	FirstName     *string    `gorm:"-" json:"first_name"`
-	LastName      *string    `gorm:"-" json:"last_name"`
-	Email         *string    `gorm:"-" json:"email"`
-	ApplicationID *uuid.UUID `gorm:"column:prvd_application_id" json:"prvd_application_id"`
-	UserID        *uuid.UUID `gorm:"column:prvd_user_id" json:"prvd_user_id"`
+	Name           *string    `json:"name"`
+	FirstName      *string    `gorm:"-" json:"first_name"`
+	LastName       *string    `gorm:"-" json:"last_name"`
+	Email          *string    `gorm:"-" json:"email"`
+	ApplicationID  *uuid.UUID `gorm:"column:prvd_application_id" json:"prvd_application_id"`
+	OrganizationID *uuid.UUID `gorm:"column:prvd_organization_id" json:"prvd_organization_id"`
+	UserID         *uuid.UUID `gorm:"column:prvd_user_id" json:"prvd_user_id"`
 }
 
 func (siaAccount) TableName() string {
@@ -121,6 +123,17 @@ func (siaApplication) TableName() string {
 	return "applications"
 }
 
+type siaOrganization struct {
+	SiaModel
+	Name           *string    `json:"name"`
+	AccountID      *uint      `json:"account_id"`
+	OrganizationID *uuid.UUID `gorm:"column:prvd_organization_id" json:"prvd_organization_id"`
+}
+
+func (siaOrganization) TableName() string {
+	return "organizations"
+}
+
 // SiaModel is only exported to workaround a bug in the gorm library
 type SiaModel struct {
 	ID     uint         `gorm:"primary_key"`
@@ -138,7 +151,7 @@ func (call *siaAPICall) enrich(db *gorm.DB) {
 
 	isApplicationSub := false
 	isUserSub := false
-	// isOrgSub := false
+	isOrgSub := false
 
 	var _sub string
 	if _sub == "" {
@@ -157,7 +170,7 @@ func (call *siaAPICall) enrich(db *gorm.DB) {
 
 		isApplicationSub = subjectParts[0] == "application"
 		isUserSub = subjectParts[0] == "user"
-		// FIXME? isOrgSub := subjectParts[0] == "organization"
+		isOrgSub = subjectParts[0] == "organization"
 	}
 
 	account := &siaAccount{} // responsible billing account
@@ -165,34 +178,59 @@ func (call *siaAPICall) enrich(db *gorm.DB) {
 
 	resolveApplication := func(applicationUUID uuid.UUID) error {
 		var resolveApplicationErr error
-		common.Log.Debugf("Resolving responsible account from call db for application: %s", applicationUUID)
+		common.Log.Debugf("resolving responsible account from call db for application: %s", applicationUUID)
 		application := &siaApplication{}
 		db.Where("prvd_application_id = ?", applicationUUID).Find(&application)
 		if application != nil && application.ID != 0 && application.UserID != nil && *application.UserID != uuid.Nil {
 			call.ApplicationID = &application.ID
-			common.Log.Debugf("Resolving responsible application owner's account from call db for user: %s; application id: %s", application.UserID, application.ApplicationID)
+			common.Log.Debugf("resolving responsible application owner's account from call db for user: %s; application id: %s", application.UserID, application.ApplicationID)
 			db.Where("prvd_user_id = ?", application.UserID).Find(&account)
 		} else if application != nil && application.ID != 0 {
-			resolveApplicationErr = fmt.Errorf("Failed to resolve responsible application owner's account from call db for user: %s; application id: %s", application.UserID, application.ApplicationID)
+			resolveApplicationErr = fmt.Errorf("failed to resolve responsible application owner's account from call db for user: %s; application id: %s", application.UserID, application.ApplicationID)
 		} else {
-			resolveApplicationErr = fmt.Errorf("Failed to resolve responsible application: %s", applicationUUID)
+			resolveApplicationErr = fmt.Errorf("failed to resolve responsible application: %s", applicationUUID)
 		}
 		return resolveApplicationErr
+	}
+
+	resolveOrganization := func(organizationUUID uuid.UUID) error {
+		var resolveOrganizationErr error
+		common.Log.Debugf("resolving responsible account from sia db for organization: %s", organizationUUID)
+		organization := &siaOrganization{}
+		db.Where("prvd_organization_id = ?", organizationUUID).Find(&organization)
+		if organization != nil && organization.ID != 0 {
+			call.OrganizationID = &organization.ID
+			common.Log.Debugf("resolving responsible organization account from sia db for organization id: %s", organization.OrganizationID)
+			db.Where("id = ?", organization.AccountID).Find(&account)
+		} else if organization != nil && organization.ID != 0 {
+			resolveOrganizationErr = fmt.Errorf("failed to resolve responsible organization account from sia db for organization: %s", organization.OrganizationID)
+		} else {
+			resolveOrganizationErr = fmt.Errorf("failed to resolve responsible organization: %s", organizationUUID)
+		}
+		return resolveOrganizationErr
 	}
 
 	if isApplicationSub {
 		applicationID := subjectParts[1]
 		applicationUUID, err := uuid.FromString(applicationID)
 		if err != nil {
-			resolverErr = fmt.Errorf("Failed to resolve responsible application; %s", err.Error())
+			resolverErr = fmt.Errorf("failed to resolve responsible application; %s", err.Error())
 		} else {
 			resolverErr = resolveApplication(applicationUUID)
+		}
+	} else if isOrgSub {
+		organizationID := subjectParts[1]
+		organizationUUID, err := uuid.FromString(organizationID)
+		if err != nil {
+			resolverErr = fmt.Errorf("failed to resolve responsible organization; %s", err.Error())
+		} else {
+			resolverErr = resolveOrganization(organizationUUID)
 		}
 	} else if isUserSub {
 		userID := subjectParts[1]
 		userUUID, err := uuid.FromString(userID)
 		if err == nil {
-			common.Log.Debugf("Resolving responsible account from call db for user: %s", userUUID)
+			common.Log.Debugf("resolving responsible account from sia db for user: %s", userUUID)
 			db.Where("prvd_user_id = ?", userUUID).Find(&account)
 
 			if account != nil && account.ApplicationID != nil && *account.ApplicationID != uuid.Nil {
@@ -202,14 +240,14 @@ func (call *siaAPICall) enrich(db *gorm.DB) {
 	}
 
 	if account == nil || account.ID == 0 {
-		resolverErr = fmt.Errorf("Failed to resolve responsible account for API call event: %s", *call.Hash)
+		resolverErr = fmt.Errorf("failed to resolve responsible account for API call event: %s", *call.Hash)
 	}
 
 	if resolverErr != nil {
-		common.Log.Warningf("Failed to persist API call event: %s; %s", *call.Hash, resolverErr.Error())
+		common.Log.Warningf("failed to persist API call event: %s; %s", *call.Hash, resolverErr.Error())
 		return
 	}
 
 	call.AccountID = &account.ID
-	common.Log.Debugf("Resolved responsible account %d for API call event: %s", *call.AccountID, *call.Hash)
+	common.Log.Debugf("resolved responsible account %d for API call event: %s", *call.AccountID, *call.Hash)
 }
